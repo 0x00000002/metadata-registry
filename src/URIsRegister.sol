4346043d096aaef620dec275f61018e3a7d08a33
Alex.Tikonoff <tikonoff@gmail.com>
2024-07-18 21:00:34 +1200

    cleanup

--- src/URIsRegister.sol
+++ src/URIsRegister.sol
@@ -12,6 +12,7 @@ import "@ipfs-cid-solidity/IPFS.sol";
 string constant IPFS_URI = "ipfs://";
 
 error UriExists(bytes32 token, bytes32 label);
+error LabelExists(address contractAddress, bytes32 label);
 
 contract URIsRegister is IPFS {
     /**
@@ -19,45 +20,72 @@ contract URIsRegister is IPFS {
      * @notice Labels are used by creators to "mark" their URIs.
      * @notice Label is an arbitrary bytes32 value.
      *
-     * @dev MRTokenId = keccak256(contractAddress, tokenId)
-     * @dev digest = keccak256(MRTokenId, attrId)
+     * @dev MRTokenId = keccak256(contractAddress,tokenId)
+     * @dev digest = keccak256(token,label)
      * @dev uri = 'ipfs://' + cidv0(digest)
      *
      * @notice we use bytes32 over the string to save gas
      */
 
-    mapping(bytes32 MRTokenId => mapping(bytes32 attrId => bytes32 keccak256hash))
-        private _hash;
-
-    // mapping(address contractAddress => bytes32[]) private _labels;
+    mapping(bytes32 MRTokenId => mapping(bytes32 label => bytes32))
+        private _digest;
+    mapping(address contractAddress => bytes32[]) private _labels;
 
     /**
      * @notice Get all token uris associated with a particular token
      * @param MRTokenId The identifier for the token
-     * @param attrId The identifier for the uri
+     * @param label The identifier for the uri
      * @return uri string
      */
     function _getURI(
         bytes32 MRTokenId,
-        bytes32 attrId
+        bytes32 label
     ) internal view returns (string memory) {
         return
-            string(abi.encodePacked(IPFS_URI, cidv1(_hash[MRTokenId][attrId])));
+            string(
+                abi.encodePacked(IPFS_URI, cidv1(_digest[MRTokenId][label]))
+            );
+    }
+
+    /**
+     * @notice Get all labels associated with a particular contract
+     * @param contractAddress The address of the contract
+     * @return labels Array of labels
+     */
+    function _getLabels(
+        address contractAddress
+    ) internal view returns (bytes32[] memory) {
+        return _labels[contractAddress];
+    }
+
+    /**
+     * @notice Add a new label to the contract
+     * @param contractAddress The address of the contract
+     * @param label The identifier for the uri
+     */
+    function _addLabel(address contractAddress, bytes32 label) internal {
+        for (uint256 i = 0; i < _labels[contractAddress].length; i++) {
+            if (_labels[contractAddress][i] == label) {
+                revert LabelExists(contractAddress, label);
+            }
+        }
+        _labels[contractAddress].push(label);
     }
 
     /**
      * @notice This function sets the URI for a given token.
-     * @param MRTokenId The MR token identifier
-     * @param attrId The attribute identifier for the token
+     * @param MRTokenId The identifier for the token
+     * @param label The identifier for the uri
      * @param digest The IPFS digest (sha2-256 hash) of the file
+     * @dev Can be called only by pre-approved accounts (studios/creators)
      */
     function _setUri(
         bytes32 MRTokenId,
-        bytes32 attrId,
+        bytes32 label,
         bytes32 digest
     ) internal {
-        if (_hash[MRTokenId][attrId] > 0) revert UriExists(MRTokenId, attrId);
+        if (_digest[MRTokenId][label] > 0) revert UriExists(MRTokenId, label);
 
-        _hash[MRTokenId][attrId] = digest;
+        _digest[MRTokenId][label] = digest;
     }
 }
